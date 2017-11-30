df9a250e7a9ba8749a8168c463444172bf01bc00
Patrik Emilsson <patrik_emilsson@hotmail.com>
2017-11-30 17:30:12 +0100

    File structuring and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import {routing} from './app.routing';
+
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyA2V_AQd1R2lbCDfjHzAoSXgg7mNPZCzhs',
+  authDomain: 'myhouse-58a88.firebaseapp.com',
+  databaseURL: 'https://myhouse-58a88.firebaseio.com',
+  projectId: 'myhouse-58a88',
+  storageBucket: 'myhouse-58a88.appspot.com',
+  messagingSenderId: '178332113016'
+};
+
 
 
 @NgModule({
@@ -10,7 +21,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,8 @@
+import {Routes, RouterModule} from '@angular/router'
+
+const APP_ROUTES: Routes = [
+ // { path: 'login', component:  },
+
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
