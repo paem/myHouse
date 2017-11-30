491a53125eeb2a6e13c98f7a847885677153239a
Vidar <vidarcsgo@gmail.com>
2017-11-30 20:00:01 +0100

    sidenav begin

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var jquery:any;
+declare var $:any;
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
-
   constructor() { }
 
+  // Jquery test
+  title = 'hej';
+  toggleTitle(){
+    $('.title').slideToggle();
+  }
+
   ngOnInit() {
+    // using context
+    $('.context.example .ui.sidebar')
+    .sidebar({
+      context: $('.context.example .bottom.segment')
+    })
+    .sidebar('attach events', '.context.example .menu .item')
+    ;
   }
 
 }
