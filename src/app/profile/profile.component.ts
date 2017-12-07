b4230392795cdb0d228da02859fed3107705103d
Patrik Emilsson <patrik_emilsson@hotmail.com>
2017-12-07 19:08:56 +0100

    start on profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  public oldPassword: string;
+  public newPassword: string;
+  public email: string;
+  public adress: string;
   constructor() { }
-
+  emailFormControl = new FormControl('', [
+    Validators.email,
+  ]);
   ngOnInit() {
   }
 
